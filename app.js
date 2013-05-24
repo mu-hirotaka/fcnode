3c5064f8b815caea726517466e39bca53fbe9ae0
muranaka.hirotaka <hirop4@gmail.com>
2013-05-24 12:19:47 +0900

    Add twitter streaming api

--- app.js
+++ app.js
@@ -5,11 +5,12 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , settings = require('./oauth_settings');
 
 var app = express();
+var server = http.createServer(app);
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -27,9 +28,38 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+var twitter = require('ntwitter');
+var io = require('socket.io').listen(server);
+var tw = new twitter(settings);
 
-http.createServer(app).listen(app.get('port'), function(){
+var keyword = "サッカー";
+app.get('/', function(req, res) {
+	if (req.query.keyword) {
+		keyword = req.query.keyword;
+	}
+	res.render('index', {
+		keyword: keyword
+	});
+
+	tw.stream('statuses/filter', { 'track': keyword }, function (stream) {
+//	tw.stream('statuses/filter', {'track': keyword, 'locations':'122.87,24.84,153.01,46.80'}, function (stream) {
+		stream.on('data', function(data) {
+			io.sockets.emit('message', data.text);
+		});
+		stream.on('end', function(response) {
+
+			console.log("end");
+		});
+		stream.on('destroy', function(response) {
+			console.log("destroy");
+
+		});
+		stream.on('error', function(error, code) {
+			console.log("my error:" + error + ":" + code);
+		});
+	});
+});
+
+server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
---
+++ oauth_settings.js
@@ -0,0 +1,6 @@
+module.exports = {
+	'consumer_key'       : 'F7RdhQaOyVVvUaOdzCQ',
+	'consumer_secret'    : 'WqYXJisqCTPAPkZ87g9fVRhiPTuWrFRgMu1dHCvg4k',
+	'access_token_key'   : '633679587-98zEunrJrGKjLIscRAcUIxcIbtANsqEEAvixeVT3',
+	'access_token_secret': 'JzyM5GicUaPW0OoSPSZlCNwXnWMPkS26KUa0JJvM'
+}
